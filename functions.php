378aab966063bea6d0095c06c8b3c6eddd24f80e
Sam Brannon <sam@branberg.com>
2014-04-04 10:52:57 -0500

    Forgot to remove the WP updates line from functions.php

--- functions.php
+++ functions.php
@@ -260,10 +260,3 @@ function boombox_add_editor_styles() {
     add_editor_style( get_template_directory_uri() . '/library/css/custom-editor-style.css' );
 }
 add_action( 'init', 'boombox_add_editor_styles' );
-
-
-/*********************************************************************************************************
-INCLUDE AUTOMATIC UPDATES
-*********************************************************************************************************/
-require_once('wp-updates-theme.php');
-new WPUpdatesThemeUpdater_689( 'http://wp-updates.com/api/2/theme', basename( get_template_directory() ) );
