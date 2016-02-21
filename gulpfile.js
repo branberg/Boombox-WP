093e7fe28b89c9b56c996cb2145e619f57915793
Sam Brannon <sam@evolvelabs.com>
2016-02-21 16:22:43 -0600

    Finished some tweaks to get auto-versioning working for the most part. Updated package to test.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var del = require('del');
+var replace = require('gulp-replace');
+var bump = require('gulp-bump');
 var zip = require('gulp-zip');
-var p = require('./package.json');
+var fs = require('fs');
 
+// Tasks to bump version number
+gulp.task('bump-package', function(){
+  gulp.src('./package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-style', function(){
+  var config = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  gulp.src(['./theme/style.css'])
+    .pipe(replace(/(Version: )(.{5})/g, 'Version: ' + config.version))
+    .pipe(gulp.dest('./theme/'));
+});
+
+// Tasks to create a .zip theme release
 gulp.task('wipe', function(){
   return del('dist/**/*')
 });
@@ -18,13 +35,19 @@ gulp.task('pre-clean', function(){
 });
 
 gulp.task('zip', function() {
+  var config = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
   return gulp.src('dist/**/*')
-    .pipe(zip('boombox-v' + p.version + '.zip'))
+    .pipe(zip('boombox-v' + config.version + '.zip'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('post-clean', function(){
   return del(['dist/**/*', '!dist/*.zip'])
+});
+
+// Sequences
+gulp.task('bump', function(callback){
+  runSequence('bump-package', 'bump-style');
 });
 
 gulp.task('release', function(callback){
